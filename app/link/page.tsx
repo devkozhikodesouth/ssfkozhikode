1cf2d5d43b384c17b245f77e24401cfbca1d07a9
shafeequemk80 <shafeequemk80@gmail.com>
2026-07-04 00:01:31 +0530

    adeed bulk message

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -3,10 +3,21 @@
 import { useEffect } from "react";
 
 export default function Pay() {
+  const upiUrl =
+    "upi://pay?pa=shafeequemk80-2@okhdfcbank&pn=SSF%20Kozhikode&am=111&cu=INR&tn=hadya%20Fee";
+
   useEffect(() => {
-    window.location.href =
-      "upi://pay?pa=shafeequemk80-2@okhdfcbank&pn=SSF%20Kozhikode&am=111&cu=INR&tn=hadya%20Fee";
+    const timer = setTimeout(() => {
+      window.location.href = upiUrl;
+    }, 500);
+
+    return () => clearTimeout(timer);
   }, []);
 
-  return <p>Opening payment app...</p>;
+  return (
+    <div>
+      <p>If the payment app doesn't open automatically, tap below.</p>
+      <a href={upiUrl}>Open Payment App</a>
+    </div>
+  );
 }
